e35d4257fcadf9033f5e67facb2c43e7d2a24a1f
JoyGZ <42661500+JoyGZ@users.noreply.github.com>
2018-08-24 10:02:54 +0800

    My First PHP Project - init Version
    My first php project , the first version, part one.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./BootCSS/bootstrap.css">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="./BootJS/jquery-3.3.1.min.js"></script>
+<script src="./BootJS/popper.js"></script>
+<script src="./BootJS/bootstrap.js"></script>
+
+<!-- Actually Content -->
+<div class="jumbotron">
+    <?php
+    echo 'PHP Log : This page powered by [Bootstrap]!';
+    ?>
+    <h1 class="display-4">PHP测试工程主页</h1>
+    <p class="lead">在这里可以跳转到之前开发的各类工程之中，便于自己复习与体验。</p>
+    <hr class="my-4">
+    <p>这是PHP的第一个综合工程，包含了入门所需各类交互和MySQL相关各类初级操作内容。</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="./HelloPHPPages/HelloP.html" role="button">跳转</a>
+    </p>
+    <hr class="my-4">
+    <p>这是PHP的第二个工程，将会完成一个更像真正工程的项目。含有图片管理、管理台、审核等内容。</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="" role="button">跳转</a>
+    </p>
+</div>
+
+</body>
+
+</html>
