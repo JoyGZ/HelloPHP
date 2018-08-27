cfbf17ee5c92570fde6a6cbea94f529a42c9880d
JoyGZ <gz.wang@foxmail.com>
2018-08-27 17:10:05 +0800

    优化头部、尾部公共取值方式 优化取值方式 新增时区设置 优化new标签

--- LebronHighlights/LeBronIndex.php
+++ LebronHighlights/LeBronIndex.php
@@ -1,66 +1,26 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../BootCSS/bootstrap.css">
-
-    <title>詹姆斯的各种得分信息</title>
-</head>
-<body>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="../BootJS/jquery-3.3.1.min.js"></script>
-<script src="../BootJS/popper.js"></script>
-<script src="../BootJS/bootstrap.js"></script>
-
-<!-- Navi Bar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">
-        <img src="./Pics/HomePageLogo.gif" width="75" height="63" class="d-inline-block align-content-center" alt="">
-        <b>James Scoreboard</b>
-    </a>
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="./LeBronIndex.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="./AddJamesInfo.php">AddData </a>
-            </li>
-
-        </ul>
-    </div>
-</nav>
+<?php
+require_once('headpart.html');
+?>
 
 <!-- Actually Content -->
 
 <?php
+    require_once('PreVars.php');
     //connect to DB
-    $dbc=mysqli_connect('localhost','root','GZphp1q2w3e4r','gzphpdb','3306')
+    $dbc=mysqli_connect(db_host,db_user,db_password,db_name,db_port)
         or die('Error with connect to the DB!');
     $query="select * from gzphpdb.James_Info";
     //query DB
     $result=mysqli_query($dbc,$query)
         or die('Error with query DB!');
     echo '<div class="card-columns">';
-    $changecount=0;
+    //$changecount=0;
     while($row=mysqli_fetch_array($result)) {
-        $changecount=$changecount+1;
+        //$changecount=$changecount+1;
         ?>
             <div class="card">
                 <?php
-                    if(!empty($row['op_pic'])){
+                    if((!empty($row['op_pic']))||(!($row['op_pic']==''))){
                         ?>
                         <img class="card-img-top" src="./Pics/<?php echo $row['op_pic']; ?>" alt="Card image cap">
                 <?php
@@ -89,7 +49,6 @@
     echo '</div>';
 ?>
 
-
-</body>
-
-</html>
+<?php
+require_once('endpart.html');
+?>
