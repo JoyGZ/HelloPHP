75175ed1e72e35a12ebd0a921339329df75fea0d
JoyGZ <gz.wang@foxmail.com>
2018-08-27 17:10:32 +0800

    预置变量

---
+++ LebronHighlights/PreVars.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoyGZ
+ * Date: 2018/8/27
+ * Time: 下午1:52
+ */
+    //define application constants
+    define('ULJPICPATH','./Pics/');
+
+    //define database info
+    define('db_host','localhost');
+    define('db_user','root');
+    define('db_password','GZphp1q2w3e4r');
+    define('db_name','gzphpdb');
+    define('db_port','3306');
+
+    //define time zone
+    date_default_timezone_set('Asia/Shanghai');
